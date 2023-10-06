1668e7150b73b11b81cac91d3f37516cbc5409a6
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-06 12:33:54 +0300

    add entity Person

---
+++ src/main/java/by/flameksandr/spring/security/models/Person.java
@@ -0,0 +1,33 @@
+package by.flameksandr.spring.security.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Person")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty
+    @Size(min = 2, max = 100, message = "Name should be between 2 and 100 characters")
+    private String username;
+
+    @Min(value = 1900, message = "Year should be more than 1900")
+    @Column(name = "year_of_birth")
+    private int yearOfBirth;
+
+    @Column(name = "password")
+    private String password;
+
+}
